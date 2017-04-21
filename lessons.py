d70c2fe25565588586c6254f87c56d54b08a2a1b
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:01:20 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 7')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #6: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Hacer más esfuerzo nos lleva a sentir plenitud duradera.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • La Voz Verdadera nos anima a hacer esfuerzo y la Voz del Oponente nos reta a tomar el camino fácil.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Al hacer esfuerzo ganamos plenitud duradera y nos brinda sentimientos de orgullo y confianza en nosotros mismos.  El tomar el camino fácil sólo nos satisface temporalmente y nos brinda sentimientos de duda, miedo y vergüenza.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para ganar el Juego de la Vida, cada uno de nosotros debemos identificar las partes de nuestra vida en las que debemos hacer esfuerzo para sobrepasar retos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El éxito está basado en la habilidad que tengamos para sobrepasar nuestros retos, no necesariamente en él o los resultados.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Parablas nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Esfuerzo: Trabajar duro para alcanzar una meta nos lleva a sentir plenitud duradera.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Camino fácil: No hacer un esfuerzo nos lleva a sentir satisfacción momentánea.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento en el que hiciste un esfuerzo. ¿Qué hiciste? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento en el que tomaste el camino fácil. ¿Qué hiciste?  ¿Cómo te sentiste?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 7')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #6: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Hacer más esfuerzo nos lleva a sentir plenitud duradera.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • La Voz Verdadera nos anima a hacer esfuerzo y la Voz del Oponente nos reta a tomar el camino fácil.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Al hacer esfuerzo ganamos plenitud duradera y nos brinda sentimientos de orgullo y confianza en nosotros mismos.  El tomar el camino fácil sólo nos satisface temporalmente y nos brinda sentimientos de duda, miedo y vergüenza.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para ganar el Juego de la Vida, cada uno de nosotros debemos identificar las partes de nuestra vida en las que debemos hacer esfuerzo para sobrepasar retos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El éxito está basado en la habilidad que tengamos para sobrepasar nuestros retos, no necesariamente en él o los resultados.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Parablas nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Esfuerzo: Trabajar duro para alcanzar una meta nos lleva a sentir plenitud duradera.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Camino fácil: No hacer un esfuerzo nos lleva a sentir satisfacción momentánea.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento en el que hiciste un esfuerzo. ¿Qué hiciste? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento en el que tomaste el camino fácil. ¿Qué hiciste?  ¿Cómo te sentiste?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 8 (Parte 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #7: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Los comportamientos reactivos nos llevan a sentir satisfacción momentanea.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Los retos detonan sentimientos reactivos; los sentimientos reactivos nos llevan a comportamientos reactivos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Los comportamientos reactivos nos dan sólo satisfacción momentánea.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Tenemos el poder de elegir detener nuestros comportamientos reactivos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Los comportamientos reactivos son expresados de muchas maneras diferentes; cada uno de nosotros tenemos que identificar los nuestros.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para detener los comportamientos reactivos y alcanzar la plenitud duradera, tenemos que usar La Fórmula de Cuatro Pasos.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Parablas nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Sentimientos reactivos: Las emociones que experimentamos cuando tenemos un reto que hacen que la Voz del Oponente se escuche muy alto.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Comportamientos reactivos: Acciones que escogemos cuando elegimos escuchar la Voz del Oponente.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Fórmula de Cuatro Pasos: Detente, pide, relájate y comparte.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • ¿Qué pasa con la Voz del Oponente cuando te sientes reactivo?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Qué puedes hacer para ayudarte a ti mismo cuando tienes sentimientos reactivos y escucha la Voz de tu Oponente?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 8 (Parte 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #7: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Los comportamientos reactivos nos llevan a sentir satisfacción momentanea.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Los retos detonan sentimientos reactivos; los sentimientos reactivos nos llevan a comportamientos reactivos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Los comportamientos reactivos nos dan sólo satisfacción momentánea.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Tenemos el poder de elegir detener nuestros comportamientos reactivos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Los comportamientos reactivos son expresados de muchas maneras diferentes; cada uno de nosotros tenemos que identificar los nuestros.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para detener los comportamientos reactivos y alcanzar la plenitud duradera, tenemos que usar La Fórmula de Cuatro Pasos.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Parablas nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Sentimientos reactivos: Las emociones que experimentamos cuando tenemos un reto que hacen que la Voz del Oponente se escuche muy alto.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Comportamientos reactivos: Acciones que escogemos cuando elegimos escuchar la Voz del Oponente.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Fórmula de Cuatro Pasos: Detente, pide, relájate y comparte.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • ¿Qué pasa con la Voz del Oponente cuando te sientes reactivo?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Qué puedes hacer para ayudarte a ti mismo cuando tienes sentimientos reactivos y escucha la Voz de tu Oponente?')),
+)
---
+++ videos.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("7.1.ogv",
+     _("La pieza faltante"),
+     _(""),
+      ),
+    ("7.2.ogv",
+     _("Haciendo un esfuerzo"),
+     _("Parece que cuanto más damos a nuestros hijos lo aprecian menos. En esta lección vamos a descubrir el mayor regalo que podemos dar a nuestros hijos - el don de ganarse las cosas."),
+      ),
+    ("8.1.ogv",
+     _('La fórmula de 4 pasos (Parte 1)'),
+     _("Nuestras reacciones inmediatas a los retos suceden tan rápidamente que parecen imposibles de controlar y aún más difícil de superar, pero hay una fórmula que puede ayudar a detener estas reacciones."),
+     ),
+    ("8.4.ogv",
+     _('La fórmula de 4 pasos (Parte 2)'),
+     _(""),
+     ),
+)
